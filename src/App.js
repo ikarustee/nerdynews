9374ad2280312916001f894235b80159d1377c1e
Tanja Karius <tanja.karius@googlemail.com>
2021-12-15 16:30:07 +0100

    Loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import {useState, useEffect} from "react"
-
+import Loader from "./components/Loader"
 function App() {
   const [articles, setArticles] = useState([])
   const [userInput, setUserInput] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   // Create a function to get the API
   const searchResults = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=' + userInput)
-    .then((res) => res.json())
-    .then((res) => setArticles(res.hits))
+    // setIsLoading(true)
+    setTimeout(() => {
+      fetch('http://hn.algolia.com/api/v1/search?query=' + userInput)
+      .then((res) => res.json())
+      .then((res) => {
+        setArticles(res.hits);
+        setIsLoading(false)
+      });
+    }, 2000)
   }
   
   // Get the user input from the input field
@@ -21,12 +28,14 @@ function App() {
   // When the user presses 'Enter' or clicks the search button the searchResults() function will be called
   const getSearchResults = (e) => {
     e.preventDefault()
+    setIsLoading(true)
     searchResults()
     setUserInput('')
   }
-
+  
   // Loads results initially 
   useEffect(() => {
+    setIsLoading(false)
     searchResults()
   }, [])
     
@@ -46,14 +55,19 @@ function App() {
         </form>
       </div>
     </div>
-    <div className="searchresults">
-    {articles
-    .map((a) => 
-      <article key={a.objectID} className='article'>
-        <a href={a.url}>{a.title}</a>
-      </article>
+    {isLoading ? (
+      <div className="searchresults"><Loader /></div>
+    ) : (
+      <div className="searchresults">
+      {articles
+      .map((a) => 
+        <article key={a.objectID} className='article'>
+          <a href={a.url}>{a.title}</a>
+        </article>
+      )}
+      </div>
     )}
-    </div>
+
     </div>
   );
 }
