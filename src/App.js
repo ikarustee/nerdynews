93ace2d97c92b3fdc4abf947af0e8618e01cb88f
Tanja Karius <tanja.karius@googlemail.com>
2022-01-06 19:41:38 +0100

    Fine tuning

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function Search({getQuery, setActivePage}) {
       name="userquery"
       placeholder="Search ..."
       />
-      <button type="submit">
+      <button type="submit" className="search">
       <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="4.36289" cy="4.36289" r="3.86289" stroke="currentColor"></circle>
             <rect width="1.09072" height="5.21053" rx="0.545362" transform="matrix(0.707106 -0.707108 0.707106 0.707108 6.54434 7.31555)" fill="currentColor"></rect>
